5322cdfaacfb38faeb412e4917ded780ac4b585f
robzdc <robzdc@robzdc-pc.(none)>
2013-11-03 21:39:46 -0600

    Se cambio tipo de sesion de base de datos a cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-#Automobile4::Application.config.session_store :cookie_store, key: '_automobile-4_session'
+Automobile4::Application.config.session_store :cookie_store, key: '_automobile-4_session'
 
-Automobile4::Application.config.session_store :active_record_store
+#Automobile4::Application.config.session_store :active_record_store

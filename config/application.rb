cfb70bd631740a92ab00e0731e1e669a71b1efe1
robzdc <robzdc@robzdc-pc.(none)>
2013-11-30 00:06:28 -0600

    Se movieron los fonts a public; Se quito el registro de usarios;

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Automobile4
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    
     config.action_mailer.default_url_options = { host: 'autobuscalo.com' }
     config.middleware.use Rack::Deflater
 
